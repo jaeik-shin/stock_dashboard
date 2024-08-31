ef52794096c64093f20e83292e45b07ceda32fb8
DESKTOP-2HL6UR8\shin jaeik <tlswodlr998@gmail.com>
2024-08-31 10:45:00 +0900

    날짜 설정 구현하기

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ df['Date'] = pd.to_datetime(df['Date'])
 start_date = st.date_input('Start date', df['date']. min())
 end_date = st.date_input('End date', df['Date'].max())
 
-ranged_df = df[(df['Date'] >= pd.to_datetime(start_date))
-(df['Date'] <= pd.to_datetime(end_date))]
+ranged_df = df[(df['Date'] >= pd.to_datetime(start_date))&(df['Date'] <= pd.to_datetime(end_date))]
+ranged_df = ranged_df.reset_index(drop=True)
 st.table(ranged_df)
 
 if __name__ == "__main__" :
