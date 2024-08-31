e8e00d1fa84c70ea41388095685841ba509011a9
DESKTOP-2HL6UR8\shin jaeik <tlswodlr998@gmail.com>
2024-08-31 10:31:23 +0900

    no message

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 import streamlit as st
 from data_loader import load_data
 
@@ -5,7 +7,15 @@ def main():
     st.title("This is a dashboard")
     df = load_data()
 
-    st.write(df)
+# 특정 날짜 선택
+st.subheader("Select date range")
+df['Date'] = pd.to_datetime(df['Date'])
+start_date = st.date_input('Start date', df['date']. min())
+end_date = st.date_input('End date', df['Date'].max())
+
+ranged_df = df[(df['Date'] >= pd.to_datetime(start_date)) &
+(df['Date'] <= pd.to_datetime(end_date))]
+st.table(ranged_df)
 
 if __name__ == "__main__" :
     main()
